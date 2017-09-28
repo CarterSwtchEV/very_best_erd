2e5d9e9fbe810c681314e5895fc272d62ac9f6a7
Raghu Betina <raghu@firstdraft.com>
2017-09-28 01:21:06 +0000

    Direct association between users and bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,11 @@
 class Bookmark < ApplicationRecord
   # Direct associations
 
+  belongs_to :users,
+             :class_name => "User",
+             :foreign_key => "user_id",
+             :counter_cache => true
+
   # Indirect associations
 
   # Validations
