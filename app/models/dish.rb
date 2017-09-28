8d98bf809dccb06762da159c3dd4cb31ab70c6a0
Raghu Betina <raghu@firstdraft.com>
2017-09-28 01:21:09 +0000

    Direct association between cuisines and dishes

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,10 @@
 class Dish < ApplicationRecord
   # Direct associations
 
+  belongs_to :cuisines,
+             :class_name => "Cuisine",
+             :counter_cache => true
+
   # Indirect associations
 
   # Validations
