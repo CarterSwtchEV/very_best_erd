88b1058ba2643ba35ae97754d0010c936e2c1756
Raghu Betina <raghu@firstdraft.com>
2017-09-29 16:39:54 +0000

    7th draft

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -4,17 +4,16 @@ class Dish < ApplicationRecord
   has_many   :bookmarks,
              :dependent => :destroy
 
-  belongs_to :cuisines,
-             :class_name => "Cuisine",
+  belongs_to :cuisine,
              :counter_cache => true
 
   # Indirect associations
 
-  has_many   :venues,
+  has_many   :specialist_venues,
              :through => :bookmarks,
              :source => :venues
 
-  has_many   :users,
+  has_many   :fans,
              :through => :bookmarks,
              :source => :users
 
--- db/migrate/20170929163819_create_dishes.rb
+++ db/migrate/20170929163819_create_dishes.rb
@@ -1,7 +1,7 @@
 class CreateDishes < ActiveRecord::Migration
   def change
     create_table :dishes do |t|
-      t.integer :cuisines_id
+      t.integer :cuisine_id
       t.string :name
 
       t.timestamps
